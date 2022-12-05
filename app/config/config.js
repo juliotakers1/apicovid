287576a4fbf377785dda65e7bc9a8cd5416078fe
juliotakers1 <goustering@hotmail.com>
2022-12-05 12:52:18 -0600

    cambio puerto

--- app/config/config.js
+++ app/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    PORT: process.env.PORT || 3000,
+    PORT: process.env.PORT || 8080,
     //DB: process.env.DB || 'mongodb://localhost:27017/hrocas'
     DB: process.env.DB || 'mongodb+srv://JROBERTO2:jROBERTO12@censoshue.yuonm.mongodb.net/?retryWrites=true&w=majority'
 }
